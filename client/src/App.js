513733893c57678f9a96ee7f474f0a8be6aa82de
Angela Man <angela.k.man@gmail.com>
2021-07-31 23:16:47 -0500

    implemented login and signup form with apollo mutations

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,34 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// create apollo proivder
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// middleware function to retrieve token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Switch>
@@ -15,7 +40,8 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
